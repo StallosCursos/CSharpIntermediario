22526766fabe07a75796c1b0e913a13555b863cd
stallosdev <stallos@outlook.com>
2021-03-09 20:18:02 -0300

    Adicionado leitura e escrita a disco

---
+++ Exemplo 1 - Leitura Escrita a Disco/LeituraEscritaDisco/EscritaDisco/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace EscritaDisco
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            File.WriteAllText("saidasimples.txt", "Olá");
+
+            string[] linhas = File.ReadAllLines("./charcters_stats.csv").ToArray();
+            File.WriteAllLines("saidasimples.txt", linhas);
+            
+            File.AppendAllLines("saidasimples.txt", linhas);
+            File.AppendAllText("saidasimples.txt", "Ultima linha");
+
+            byte[] arquivo = File.ReadAllBytes("./charcters_stats.csv");
+            File.WriteAllBytes("saidasimples.txt", arquivo);
+
+            using (FileStream stream = File.Open("saidasimples.txt", FileMode.OpenOrCreate))
+            {
+                var bytes = Encoding.UTF8.GetBytes($"saida por stream {DateTime.Now.ToString()}");
+                stream.Write(bytes);
+                stream.Close();
+            }
+        }
+    }
+}
---
+++ Exemplo 1 - Leitura Escrita a Disco/LeituraEscritaDisco/LeituraEscritaDisco/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using ConsoleTables;
+
+namespace LeituraEscritaDisco
+{
+    class Program
+    {
+        private static Stopwatch _stopwatch = Stopwatch.StartNew();
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lendo arquivo do disco");
+
+            FileInfo fileInfo = new FileInfo("./charcters_stats.csv");
+            
+            Console.WriteLine($"Caminho completo: {fileInfo.FullName}");
+            Console.WriteLine($"Tamnho em Bytes: { fileInfo.Length }");
+            Console.WriteLine($"extensão Arquivo { fileInfo.Extension }");
+
+            if (fileInfo.Exists)
+            {
+                LeituraCarregandoArquivoInteiro();
+                LeituraSemCarregarArquivoInteiro();
+                LeituraComStreamReader();
+            }
+        }
+
+        private static void LeituraComStreamReader()
+        {
+            ConsoleTable consoleTable = new ConsoleTable();
+
+            using (StreamReader reader = new StreamReader("./charcters_stats.csv"))
+            {
+                consoleTable.AddColumn(reader.ReadLine().Split(','));
+                while (!reader.EndOfStream)
+                {
+                    consoleTable.AddRow(reader.ReadLine().Split(','));
+                }
+                consoleTable.Write();
+            }
+        }
+
+        private static void LeituraSemCarregarArquivoInteiro()
+        {
+            ConsoleTable consoleTable = new ConsoleTable();
+
+            _stopwatch.Restart();
+
+            int indexLine = 0;
+            foreach (var line in File.ReadLines("./charcters_stats.csv"))
+            {
+                if (indexLine == 0)
+                    consoleTable.AddColumn(line.Split(','));
+                else
+                    consoleTable.AddRow(line.Split(','));
+
+                indexLine++;
+            }
+            consoleTable.Write();
+
+            _stopwatch.Stop();
+            Console.WriteLine($"Tempo de execução carregando arquivo parcialmente {_stopwatch.ElapsedMilliseconds}");
+        }
+
+        private static void LeituraCarregandoArquivoInteiro()
+        {
+            ConsoleTable consoleTable = new ConsoleTable();
+
+            _stopwatch.Restart();
+
+            List<string> linhas = File.ReadAllLines("./charcters_stats.csv").ToList();
+
+            consoleTable.AddColumn(linhas[0].Split(','));
+            for (int i = 1; i < linhas.Count; i++)
+            {
+                string[] colunas = linhas[i].Split(',');
+                consoleTable.AddRow(colunas);
+            }
+
+            consoleTable.Write();
+
+            _stopwatch.Stop();
+            Console.WriteLine($"Tempo de execução carregando arquivo inteiro {_stopwatch.ElapsedMilliseconds}");
+        }
+    }
+}
