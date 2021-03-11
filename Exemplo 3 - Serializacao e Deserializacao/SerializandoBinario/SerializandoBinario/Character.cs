b0c76ed59a2e9b9901eacadbb382f6c3460114ae
stallosdev <stallos@outlook.com>
2021-03-11 08:00:05 -0300

    Adicionado Serialização binaria

---
+++ Exemplo 3 - Serializacao e Deserializacao/SerializandoBinario/SerializandoBinario/Character.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UtilizandoLinq
+{
+    [Serializable]
+    public class Character
+    {
+        public Character(string linha)
+        {
+            string[] colunas = linha.Split(',');
+
+            this.Name = colunas[0];
+            this.Alignment = colunas[1];
+            this.Intelligence = Int32.Parse(colunas[2]);
+            this.Strength = Int32.Parse(colunas[3]);
+            this.Speed = Int32.Parse(colunas[4]);
+            this.Durability = Int32.Parse(colunas[5]);
+            this.Power = Int32.Parse(colunas[6]);
+            this.Combat = Int32.Parse(colunas[7]);
+            this.Total = Int32.Parse(colunas[8]);
+        }
+
+        public string Name { get; private set; }
+        public string Alignment { get; private set; }
+        public int Intelligence { get; private set; }
+        public int Strength { get; private set; }
+        public int Speed { get; private set; }
+        public int Durability { get; private set; }
+        public int Power { get; private set; }
+        public int Combat { get; private set; }
+        public int Total { get; private set; }
+    }
+}
---
+++ Exemplo 3 - Serializacao e Deserializacao/SerializandoBinario/SerializandoBinario/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using UtilizandoLinq;
+
+namespace SerializandoBinario
+{
+    class Program: ProgramBase
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<string> Arquivo = LerArquivoCsv("./charcters_stats.csv");
+            IEnumerable<Character> characters = Arquivo.Select(t => new Character(t));
+
+            var Resultado = characters.Where(t => t.Alignment == "good");
+
+            BinaryFormatter binaryFormatter = new BinaryFormatter();
+
+            SerializandoObjeto(Resultado, binaryFormatter);
+
+            List<Character> ArquivoDeserializado;
+
+            using (FileStream ArquivoSerializado = new FileStream("./chars_bons.ser", FileMode.Open))
+            {
+                ArquivoDeserializado = (List<Character>)binaryFormatter.Deserialize(ArquivoSerializado);
+                ArquivoSerializado.Close();
+            }
+        }
+
+        private static void SerializandoObjeto(IEnumerable<Character> Resultado, BinaryFormatter binaryFormatter)
+        {
+            using (FileStream ArquivoSerializado = new FileStream("./chars_bons.ser", FileMode.Create))
+            {
+                binaryFormatter.Serialize(ArquivoSerializado, Resultado.ToList());
+                ArquivoSerializado.Close();
+            }
+        }
+    }
+}
